0b5608059d8b21b0831d5d7ecda346ae5e881582
rafimk <rafi.genius.cs@gmail.com>
2022-08-07 22:50:26 +0400

    first commit

---
+++ HomeController.cs
@@ -0,0 +1,37 @@
+﻿using HtmlToPdfDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SelectPdf;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HtmlToPdfDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+     public FileResult GeneratePdf(string html)
+        {
+            html = html.Replace("strtTag", "<").Replace("EndTag", ">");
+            HtmlToPdf objhtml = new HtmlToPdf();
+            PdfDocument objdoc = objhtml.ConvertHtmlString(html);
+            byte[] pdf = objdoc.Save();
+            objdoc.Close();
+            return File(pdf,"application/pdf","HtmlContent.pdf");
+        }
+    }
+}
